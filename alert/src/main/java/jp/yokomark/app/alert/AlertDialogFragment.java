d554f37ccf92eea7d374eab4dfbf50b267646487
Keith Yokoma <keith.yokoma@gmail.com>
2014-08-04 23:37:47 +0900

    Add alert module

--- alert/src/main/java/jp/yokomark/app/alert/AlertDialogFragment.java
+++ alert/src/main/java/jp/yokomark/app/alert/AlertDialogFragment.java
@@ -1,7 +1,16 @@
 package jp.yokomark.app.alert;
 
+import android.annotation.TargetApi;
+import android.app.DialogFragment;
+import android.os.Build;
+
 /**
- * Created by yokomakukeishin on 2014/08/04.
+ * @author KeithYokoma
  */
-public class AlertDialogFragment {
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class AlertDialogFragment extends DialogFragment {
+    private static final String ARGS_TITLE = "title";
+    private static final String ARGS_MESSAGE = "title";
+    private static final String ARGS_POSITIVE = "positive";
+    private static final String ARGS_NEGATIVE = "negative";
 }
